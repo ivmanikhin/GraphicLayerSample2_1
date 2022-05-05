536b7b36d7712c0baaaef11ebcc510cf335c2a17
ivmanikhin <ivmanikhin@gmail.com>
2022-05-05 09:53:13 +0300

    TODO: make text form

--- TextEditorView.xaml.cs
+++ TextEditorView.xaml.cs
@@ -2,39 +2,18 @@
 
 namespace Ascon.Pilot.SDK.GraphicLayerSample
 {
-    public partial class GraphicLayerElementSettingsView
+    public partial class TextEditorView
     {
-        public GraphicLayerElementSettingsView()
+        public TextEditorView()
         {
             InitializeComponent();
         }
 
         private void OnSaveButtonClicked(object sender, RoutedEventArgs e)
         {
-            var vertical = VerticalAlignment.Top;
-            var horizontal = HorizontalAlignment.Left;
-            if (LeftBottomCornerButton.IsChecked == true)
-            {
-                vertical = VerticalAlignment.Bottom;
-                horizontal = HorizontalAlignment.Left;
-            }
-            if (RightTopCornerButton.IsChecked == true)
-            {
-                vertical = VerticalAlignment.Top;
-                horizontal = HorizontalAlignment.Right;
-            }
 
-            if (RightBottomCornerButton.IsChecked == true)
-            {
-                vertical = VerticalAlignment.Bottom;
-                horizontal = HorizontalAlignment.Right;
-            }
                 
-            var model = DataContext as GraphicLayerElementSettingsModel;
-            var includeStamp = StampCheckBox.IsChecked != null && StampCheckBox.IsChecked.Value;
-
-            model.SaveSettings(PathButtonEdit.Text, TxbXOffset.Text, TxbYOffset.Text, TxbScale.Text, TxbAngle.Text, TxbPage.Text, vertical, horizontal, includeStamp);
-            Close();
+            
         }
 
         private void OnCancelButtonClicked(object sender, RoutedEventArgs e)
